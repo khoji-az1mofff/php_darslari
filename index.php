4a0d50c2670c38e84b757da4a5647bee09506a1e
Hojiakbar Azimov <147535410+khoji-az1mofff@users.noreply.github.com>
2023-12-29 16:55:36 +0500

    oddiy code

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<?php
-for ($i=0; $i <1; $i++) {
-    echo "Hojiakbar qalesan";
-    echo "yaxshi";
-}
-if (2==2) {
-    echo "2 = 2 ga";
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mening php codlarim</title>
+</head>
+<body>
+     <h1>
+        <?php 
+            $salomlash="ASSALOMU ALAYKUM";
+            for ($i=0; $i < 10; $i++) { 
+                echo $salomlash;
+            }
+        ?>
+     </h1>
+</body> 
+</html>
